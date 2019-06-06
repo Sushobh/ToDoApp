b7461a854e8288dabaac019f30e0acc0ad8ae159
Sushobh Nadiger <aryansushobh@gmail.com>
2019-06-06 21:21:40 +0530

    Added firestore

--- app/src/main/java/org/sushobh/exampleapp/MainActivity.kt
+++ app/src/main/java/org/sushobh/exampleapp/MainActivity.kt
@@ -2,6 +2,8 @@ package org.sushobh.exampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import org.sushobh.exampleapp.Base.BaseActivity
 import org.sushobh.exampleapp.Dagger.Components.DaggerActivityBuilder
 import org.sushobh.exampleapp.Dagger.Modules.ViewModelModule
@@ -23,7 +25,24 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel.toString()
+
+
+        val db = FirebaseFirestore.getInstance()
+
+        val user = HashMap<String, Any>()
+        user["first"] = "Ada"
+        user["last"] = "Lovelace"
+        user["born"] = 1815
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("123", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("123", "Error adding document", e)
+            }
+
     }
 
 
